c31c84b1dd4af800cc270fb0fa51127a963ec5a2
Dylan Richards <dylanrichards@Dylans-MBP.home>
2015-12-16 12:16:53 -0500

    Refactor the JSON.parse method

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -10,7 +10,7 @@ describe Api::V1::UsersController do
     end
 
     it 'returns the information about a reporter on a hash' do
-      user_response = JSON.parse(response.body, symbolize_names: true)
+      user_response = json_response
       expect(user_response[:email]).to eq @user.email
     end
 
@@ -25,7 +25,7 @@ describe Api::V1::UsersController do
       end
 
       it 'renders the json representation for the user record just created' do
-        user_response = JSON.parse(response.body, symbolize_names: true)
+        user_response = json_response
         expect(user_response[:email]).to eq @user_attributes[:email]
       end
 
@@ -39,12 +39,12 @@ describe Api::V1::UsersController do
       end
 
       it 'renders an error in json' do
-        user_response = JSON.parse(response.body, symbolize_names: true)
+        user_response = json_response
         expect(user_response).to have_key(:errors)
       end
 
       it 'renders a json error regarding why the user could not be created' do
-        user_response = JSON.parse(response.body, symbolize_names: true)
+        user_response = json_response
         expect(user_response[:errors][:email]).to include "can't be blank"
       end
 
