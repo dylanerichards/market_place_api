5e68b9a8a2de12ed1c2b29967ad2a02b15a0b5de
Dylan Richards <dylanrichards@Dylans-MBP.home>
2015-12-16 12:31:40 -0500

    Refactor test headers for each request

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -2,12 +2,10 @@ require 'spec_helper'
 
 describe Api::V1::UsersController do
 
-  before(:each) { request.headers['Accept'] = 'application/vnd.marketplace.v1' }
-
   describe "GET #show" do
     before(:each) do
       @user = FactoryGirl.create(:user)
-      get :show, id: @user.id, format: :json
+      get :show, id: @user.id
     end
 
     it 'returns the information about a reporter on a hash' do
@@ -22,7 +20,7 @@ describe Api::V1::UsersController do
     context "when it is successfully created" do
       before(:each) do
         @user_attributes = FactoryGirl.attributes_for :user
-        post :create, { user: @user_attributes }, format: :json
+        post :create, { user: @user_attributes }
       end
 
       it 'renders the json representation for the user record just created' do
@@ -36,7 +34,7 @@ describe Api::V1::UsersController do
     context "when it is not created" do
       before(:each) do
         @invalid_user_attributes = { password: "12345678", password_confirmation: "12345678" }
-        post :create, { user: @invalid_user_attributes }, format: :json
+        post :create, { user: @invalid_user_attributes }
       end
 
       it 'renders an error in json' do
@@ -56,7 +54,7 @@ describe Api::V1::UsersController do
   describe "DELETE #destroy" do
     before(:each) do
       @user = FactoryGirl.create(:user)
-      delete :destroy, { id: @user.id }, format: :json
+      delete :destroy, { id: @user.id }
     end
 
     it { should respond_with 204 }
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,5 +12,11 @@ RSpec.configure do |config|
   config.infer_base_class_for_anonymous_controllers = false
   config.order = "random"
   config.infer_spec_type_from_file_location!
+
   config.include Request::JsonHelpers, type: :controller
+  config.include Request::HeadersHelpers, type: :controller
+
+  config.before(:each, type: :controller) do
+    include_default_accept_headers
+  end
 end
